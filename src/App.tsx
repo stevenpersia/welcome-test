d50618312c327e0ff9e67b3e453cf2edbab11f10
Steven Persia <persia.steven8@gmail.com>
2021-11-16 13:35:26 +0100

    feat(App): fetch data and add them to store

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useReq from "./hooks/useReq";
+import { useApp } from "./store";
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+const API_URL =
+  "https://www.welcomekit.co/api/v1/embed?organization_reference=Pg4eV6k";
+
+const App = () => {
+  const { dispatch } = useApp();
+  const { response, status } = useReq({ url: API_URL });
+
+  useEffect(() => {
+    dispatch({ type: "GET_ORGA_OFFERS", payload: response });
+  }, [dispatch, response]);
+
+  if (status === "LOADING") return <div>Loading...</div>;
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
 
 export default App;
