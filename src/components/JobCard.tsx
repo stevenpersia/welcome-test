1b124f9b868384d35dbc67b758695675d893aea8
Steven Persia <persia.steven8@gmail.com>
2021-11-16 18:02:19 +0100

    refactor(JobCard): improve JobCard component

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -6,56 +6,57 @@ import { Text } from "@welcome-ui/text";
 
 import { IJob } from "../utils/interfaces";
 
-const JobCard = ({ data, name }: { data: IJob; name: string }) => (
-  <Box
-    backgroundColor="light.900"
-    display="flex"
-    margin="xl"
-    position="relative"
-  >
+const JobCard = ({ data, name }: { data: IJob; name: string }) => {
+  return (
     <Box
-      backgroundColor="primary.500"
+      backgroundColor="light.900"
       display="flex"
-      alignItems="center"
-      padding="xl"
+      margin="xl"
+      position="relative"
     >
-      <Text variant="h3" color="light.900">
-        {name}
-      </Text>
-    </Box>
-
-    <Box padding="xl">
-      <Box display="flex" alignItems="center" justifyContent="space-between">
-        <Text my="0" variant="body3" color="dark.100">
-          Déjà vu
+      <Box
+        backgroundColor="primary.500"
+        display="flex"
+        alignItems="center"
+        padding="xl"
+      >
+        <Text variant="h3" color="light.900">
+          {name}
         </Text>
       </Box>
-
-      <Badge position="absolute" right={16} top={16} variant="primary">
-        New
-      </Badge>
-
-      <Text variant="h4" mb={8} mt={4} lines={1}>
-        {data.name}
-      </Text>
-
-      <Stack direction="row" spacing="xl">
-        <Box display="flex" alignItems="center">
-          <CompassIcon color="primary.500" title="Type" />
-          <Text color="dark.200" my={0} variant="body3">
-            {data.contract_type.en}
+      <Box padding="xl">
+        <Box display="flex" alignItems="center" justifyContent="space-between">
+          <Text color="dark.100" my="0" variant="body3">
+            Already seen
           </Text>
         </Box>
 
-        <Box display="flex" alignItems="center">
-          <LocationIcon color="primary.500" title="Location" />
-          <Text color="dark.200" my={0} variant="body3">
-            {data.office.name}
-          </Text>
-        </Box>
-      </Stack>
+        <Badge position="absolute" right={16} top={16} variant="primary">
+          New
+        </Badge>
+
+        <Text lines={1} mb={8} mt={4} variant="h4">
+          {data.name}
+        </Text>
+
+        <Stack direction="row" spacing="xl">
+          <Box display="flex" alignItems="center">
+            <CompassIcon color="primary.500" title="Type" />
+            <Text color="dark.200" my={0} variant="body3">
+              {data.contract_type.en}
+            </Text>
+          </Box>
+
+          <Box display="flex" alignItems="center">
+            <LocationIcon color="primary.500" title="Location" />
+            <Text color="dark.200" my={0} variant="body3">
+              {data.office.name}
+            </Text>
+          </Box>
+        </Stack>
+      </Box>
     </Box>
-  </Box>
-);
+  );
+};
 
 export default JobCard;
