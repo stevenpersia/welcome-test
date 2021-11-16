f27386ef680bd6a7ce72f7909d274b84012f9f22
Steven Persia <persia.steven8@gmail.com>
2021-11-16 13:33:51 +0100

    feat(useReq): create custom hook to do some http requests

---
+++ src/hooks/useReq.ts
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+interface IUseReq {
+  method?: "GET" | "POST" | "PUT" | "DELETE";
+  url: string;
+}
+
+const useReq = ({ method = "GET", url }: IUseReq) => {
+  const initOpt = { url, method, body: undefined, headers: undefined };
+  const [options, setOptions] = useState(initOpt);
+  const [status, setStatus] = useState("IDLE"); // IDLE | LOADING
+  const [response, setResponse] = useState() as any;
+
+  useEffect(() => {
+    const fetchInfo = async () => {
+      setStatus("LOADING");
+
+      const res = await fetch(options.url, {
+        method: options.method,
+        body: options.body,
+        headers: options.headers,
+      });
+
+      const resJson = await res.json();
+      setResponse(resJson);
+
+      setStatus("IDLE");
+    };
+
+    fetchInfo();
+  }, [options]);
+
+  return { status, response, fetch: setOptions };
+};
+
+export default useReq;
