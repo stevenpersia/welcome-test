a2eb08336c1156acebdeca21e03b53e5e2765554
Steven Persia <persia.steven8@gmail.com>
2021-11-17 18:11:34 +0100

    feat(App): handle loading

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@ import { Box } from "@welcome-ui/box";
 
 import useReq from "./hooks/useReq";
 import { useApp } from "./store";
-import { Modal, ResultsList, ResultsTag, SearchBar } from "./components";
+import {
+  Loader,
+  Modal,
+  ResultsList,
+  ResultsTag,
+  SearchBar,
+} from "./components";
 
 const API_URL =
   "https://www.welcomekit.co/api/v1/embed?organization_reference=Pg4eV6k";
@@ -16,13 +22,11 @@ const App = () => {
     dispatch({ type: "GET_ORGA_OFFERS", payload: response });
   }, [dispatch, response]);
 
-  if (status === "LOADING") return <div>Loading...</div>;
-
   return (
     <Box margin="0 auto" maxWidth={720}>
       <ResultsTag />
       <SearchBar />
-      <ResultsList />
+      {status === "LOADING" ? <Loader /> : <ResultsList />}
       <Modal />
     </Box>
   );
